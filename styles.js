2e8811019e4cfbea5667ede08f605fd3d1b37cce
Annette Wöhler <annettewoehler@gmx.de>
2024-02-04 12:38:42 +0100

    Initial commit

---
+++ styles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: system-ui;
+  }
+`;
